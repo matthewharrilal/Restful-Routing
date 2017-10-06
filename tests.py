e763cdbf0d6cca2a13fcf5e1eec3853c64125a89
matthewharrilal <matthewharrilal@gmail.com>
2017-10-06 10:42:15 -0700

    Finished testing for the creation of users, starting the process of CRUD

--- tests.py
+++ tests.py
@@ -4,36 +4,39 @@ import json
 import bcrypt
 import base64
 from pymongo import MongoClient
-import Restful-Routing-Exercises
+from RestfulRoutingExercises import app
+import pdb
 
-class TriplPlannerTestCase(unittest.TestCase):
+
+
+class FlaskrTestCase(unittest.TestCase):
     def setUp(self):
         #  From my assumption this is another keyword that we inherit from the test case
-        self.app = server.app.test_client()
+        self.app = app.test_client()
 
-        server.app.config['TESTING'] = True
+        app.config['TESTING'] = True
 
         '''So essentially what we are doing here is that we are setting up our client and we are
         making our database global'''
         mongo = MongoClient('localhost', 27017)
         global db
         # Reduce encryption workloads for tests
-        server.app.bcrypt_rounds = 4
+        app.bcrypt_rounds = 4
 
         # The creation of our database
 
         db = mongo.trip_planner_test
 
         #  Creation of our server
-        server.app.db = db
+        app.db = db
 
         # We do this to clear our database before each test runs
         db.drop_collection('posts')
 
         # Here we create the tests for the users as well as fill this function with methods
 
-        def test_getting_a_user(self):
-            self.app.post('/route',
+    def test_getting_a_user(self):
+        self.app.post('/route',
                             headers=None,
                             data=json.dumps({"name":"Matthew",
                                             "student_id": "Personality",
@@ -41,14 +44,16 @@ class TriplPlannerTestCase(unittest.TestCase):
                                             content_type='application/json')
             # Make a get request to fetch the users to check if the user is actually getting created
 
-            response = self.app.get('/route',
-                                    query_string=dict(student_id="Personality"))
+        response = self.app.get('/route',
+                                    query_string=dict(name="Matthew"))
 
             # When we get the repsonse we then want to decode it so it becomes a native object
-            repsonse_json = json.loads(response.data.decode())
+        repsonse_json = json.loads(response.data.decode())
+        print(response)
+
 
             # Here we actually test if the get request was successfull
-            self.assertEqual(response.status_code,200)
+        self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
